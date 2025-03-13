b14f0d8be14028c6ef5d34f9a58d92a35bade611
Lucas <154390549+LucasVince@users.noreply.github.com>
2025-03-13 10:54:35 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import Timer from './components/timer';
-import { useState } from 'react';
+import Timer from './components/Timer';
+import { useEffect, useState } from 'react';
 
 const App = () => {
 
@@ -21,6 +21,10 @@ const App = () => {
     on: false
   });
 
+  useEffect(() => {
+    console.log(timer);
+  }, [timer]);
+
   const updateTimer = (newTime: number) => {
     const clampedTime = Math.min(Math.max(newTime, 0), MAX_TIME);
     const newMinutes = Math.min(Math.max(Math.floor(clampedTime / 60000), 0), MAX_MINUTES);
@@ -39,9 +43,57 @@ const App = () => {
   const subSecond = () => updateTimer(timer.time - 1000);
   const subMinute = () => updateTimer(timer.time - 60000);
 
+  const onStartClick = () => {
+    const newTimer = {
+      ...timer,
+      on: !timer.on
+    }
+
+    setTimer(newTimer);
+
+    let subSecondsInterval:number | undefined;
+
+    if (newTimer.on) {
+      subSecondsInterval = setInterval(() => {    
+        setTimer((timer) => {
+          let newTimer = {
+            ...timer,
+            formatTimeSecond: timer.formatTimeSecond - 1
+          };
+
+          if (newTimer.formatTimeSecond < 0) {
+            let newTimer = {
+              ...timer,
+              formatTimeSecond: 59,
+              formatTimeMinute: timer.formatTimeMinute - 1
+            };
+
+            return newTimer;
+          }
+
+          return newTimer;
+        });
+      }, 1000);
+
+      setTimeout(() => {
+        let newTimer = {
+          ...timer,
+          on: false
+        }
+    
+        setTimer(newTimer);
+        clearInterval(subSecondsInterval);
+      }, newTimer.time);
+    } else {
+      if (subSecondsInterval) {
+        clearInterval(subSecondsInterval);
+      }
+    }
+  }
+
   return (
     <div className="flex flex-col gap-10 w-screen h-screen items-center justify-center bg-red-500">
-      <Timer onAddSecond={addSecond} onAddMinute={addMinute} onSubSecond={subSecond} onSubMinute={subMinute} timeSecond={timer.formatTimeSecond} timeMinute={timer.formatTimeMinute}></Timer>
+      <Timer onAddSecond={addSecond} onAddMinute={addMinute} onSubSecond={subSecond} onSubMinute={subMinute} timeSecond={timer.formatTimeSecond} timeMinute={timer.formatTimeMinute} onStartClick={onStartClick} isTimerOn={timer.on}></Timer>
     </div>
   )
 }
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -9,23 +9,26 @@ interface TimerProps {
     onSubMinute: () => void;
     timeSecond: number;
     timeMinute: number;
+    onStartClick: () => void;
+    isTimerOn: boolean;
 }
 
-const Timer: FunctionComponent<TimerProps> = ({onAddSecond, onAddMinute, onSubSecond, onSubMinute, timeMinute, timeSecond}) => {
+const Timer: FunctionComponent<TimerProps> = (props) => {
     return (
         <div className="bg-red-300 py-8 px-12 rounded-lg text-white flex flex-col gap-5">
+            <p className="">Select the time you want for studing</p>
             <div className="flex gap-5">
                 <div className="flex flex-col gap-2">
-                    <ButtonControlTime onClick={onSubMinute}><Minus /></ButtonControlTime>
-                    <ButtonControlTime onClick={onSubSecond}><Minus /></ButtonControlTime>
+                    <ButtonControlTime onClick={props.onSubMinute}><Minus /></ButtonControlTime>
+                    <ButtonControlTime onClick={props.onSubSecond}><Minus /></ButtonControlTime>
                 </div>
-                <p className="text-6xl">{String(timeMinute).length == 2? timeMinute : "0"+timeMinute}:{String(timeSecond).length == 2? timeSecond : "0"+timeSecond}</p>
+                <p className="text-6xl">{String(props.timeMinute).length == 2? props.timeMinute : "0"+props.timeMinute}:{String(props.timeSecond).length == 2? props.timeSecond : "0"+props.timeSecond}</p>
                 <div className="flex flex-col gap-2">
-                    <ButtonControlTime onClick={onAddMinute}><Plus /></ButtonControlTime>
-                    <ButtonControlTime onClick={onAddSecond}><Plus /></ButtonControlTime>
+                    <ButtonControlTime onClick={props.onAddMinute}><Plus /></ButtonControlTime>
+                    <ButtonControlTime onClick={props.onAddSecond}><Plus /></ButtonControlTime>
                 </div>
             </div>
-            <button onClick={() => console.log('clicked')} className="text-3xl bg-red-200 text-red-600 rounded-md p-1 shadow-md hover:bg-red-600 hover:text-white">Start</button>
+            <button onClick={props.onStartClick} className="text-3xl bg-red-200 text-red-600 rounded-md p-1 shadow-md hover:bg-red-600 hover:text-white">{props.isTimerOn? 'Stop' : 'Start'}</button>
         </div>
     );
 }
